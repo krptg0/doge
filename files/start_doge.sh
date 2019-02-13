261f787f402f8255e6622ec92164e60a23a7f99b
krptg0 <krptg0@gmail.com>
2019-02-13 11:12:48 +0100

    changed default paths 2019-complient (kek)

--- files/start_doge.sh
+++ files/start_doge.sh
@@ -30,23 +30,23 @@ if [ -n "${DOGE_PASSWD}" ]; then
 fi
 
 if [ -n "${BASE_PATH}" ]; then
-  sed -i -e 's/\/nsp\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${BASE_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
+  sed -i -e 's/\/nsp\/base\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${BASE_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
   echo "BASE path set to /nsp/${BASE_PATH}"
 fi
 
 
 if [ -n "${UPDATE_PATH}" ]; then
-  sed -i -e 's/\/nsp\/Updates\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${UPDATE_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
+  sed -i -e 's/\/nsp\/updates\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${UPDATE_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
   echo "UPDATE path set to /nsp/${UPDATE_PATH}"
 fi
 
 if [ -n "${DLC_PATH}" ]; then
-  sed -i -e 's/\/nsp\/DLC\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${DLC_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
+  sed -i -e 's/\/nsp\/dlc\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${DLC_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
   echo "DLC path set to /nsp/${DLC_PATH}"
 fi
 
 if [ -n "${NSX_PATH}" ]; then
-  sed -i -e 's/\/nsp\/NSX\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${NSX_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
+  sed -i -e 's/\/nsp\/nsx_base\/{name}\[{id}\]\[v{version}\].nsp/\/nsp\/'${NSX_PATH}'{name}[{id}][v{version}].nsp/g' /doge/nut/conf/nut.conf
   echo "NSX path set to /nsp/${NSX_PATH}"
 fi
 
